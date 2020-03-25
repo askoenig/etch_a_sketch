6df2bd66c8ba9c9d7fe99a9acf75b139904e0765
askoenig <askoenig94@gmail.com>
2020-03-25 16:24:31 -0500

    I spent so long working on this without committing.. I'm almost done with the final project, will update from this point on

---
+++ script.js
@@ -0,0 +1,35 @@
+const grid = document.querySelector(".gridContainer");
+let input = document.getElementById("quantity");
+
+// const newSize = input.value;
+
+createGrid = () => {
+  input.addEventListener("change", updateGrid);
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+};
+
+updateGrid = () => {
+  grid.innerHTML = "";
+  grid.style.setProperty(
+    "grid-template-columns",
+    `repeat(${input.value}, 2fr)`
+  );
+  grid.style.setProperty("grid-template-rows", `repeat(${input.value}, 2fr)`);
+  for (let i = 0; i < input.value * input.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+  console.log(input.value);
+};
+
+const square = document.querySelector("div");
+square.addEventListener("mousedown", function(event) {
+  event.target.classList.replace("square", "color");
+});
+
+createGrid();
