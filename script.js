663e83766727569fdc7a788b169e9302be9190be
askoenig <askoenig94@gmail.com>
2020-03-29 13:11:27 -0500

    cleaning up code/comments

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const grid = document.querySelector(".gridContainer");
-const input = document.getElementById("quantity");
-const button = document.querySelector(".reset");
-
-// button.addEventListener("click", clearGrid);
+const userInput = document.getElementById("quantity");
+const resetButton = document.querySelector(".reset");
 
 createGrid = () => {
-  input.addEventListener("change", updateGrid);
   for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
@@ -17,15 +14,17 @@ updateGrid = () => {
   grid.innerHTML = "";
   grid.style.setProperty(
     "grid-template-columns",
-    `repeat(${input.value}, 2fr)`
+    `repeat(${userInput.value}, 2fr)`
   );
-  grid.style.setProperty("grid-template-rows", `repeat(${input.value}, 2fr)`);
-  for (let i = 0; i < input.value * input.value; i++) {
+  grid.style.setProperty(
+    "grid-template-rows",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  for (let i = 0; i < userInput.value * userInput.value; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
     grid.appendChild(div);
   }
-  console.log(input.value);
 };
 
 const square = document.querySelector("div");
@@ -33,9 +32,11 @@ square.addEventListener("mouseover", function(event) {
   event.target.classList.replace("square", "color");
 });
 
-button.addEventListener("click", function() {
+userInput.addEventListener("change", updateGrid);
+
+resetButton.addEventListener("click", function() {
   grid.innerHTML = "";
-  input.value = "";
+  userInput.value = "";
   grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
   grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
   createGrid();
